140a1fc72ec4911e56ce20aaf1532e75e59660ab
Taj Sharma <sharma.tajinder01@gmail.com>
2024-02-09 01:32:09 -0800

    move script call to the end of the html

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ const gridsize = 20;
 for(let i = 0; i<gridsize; i++){
     for(let j = 0; j<gridsize; j++){
         const tile = document.createElement('div'); //create div for 1 tile
-        tile.classList.add('tile')                  //assign a classname to the tile so we can style it
+        tile.classList.add('tile');             //assign a classname to the tile so we can style it
         tile.style.height = '20px';
         tile.style.width = '20px';
-
+        console.log(tile)
+        container.appendChild(tile); //adds our created tile to the grid
     }
+    container.appendChild(document.createElement('br')); //add breakline after each row is complete
 }
