e2b05950a8c117d9678718e04e3a429058566587
Taj Sharma <sharma.tajinder01@gmail.com>
2024-02-09 13:56:09 -0800

    add event listenr to each tile and change sizing/dimensions of grid

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 const container = document.querySelector('.gridHolder');
 
-const gridsize = 16;
+const gridsize = 20;
 
 for(let i = 0; i<gridsize; i++){
     for(let j = 0; j<gridsize; j++){
         const tile = document.createElement('div'); //create div for 1 tile
         tile.classList.add('tile');             //assign a classname to the tile so we can style it
-        tile.style.height = '28px';
-        tile.style.width = '28px';
+        tile.style.height = '20px';
+        tile.style.width = '20px';
         container.appendChild(tile); //adds our created tile to the grid
     }
     container.appendChild(document.createElement('br')); //add breakline after each row is complete
 }
+
+const tiles = document.querySelectorAll('.tile');
+tiles.forEach(tile => {
+    tile.addEventListener('mouseover', () => {
+        tile.style.backgroundColor = 'purple';
+    });
+});
