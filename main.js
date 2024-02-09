6bf26ee813ca72100c392f283bbb1d83bf50e1d5
Taj Sharma <sharma.tajinder01@gmail.com>
2024-02-09 13:41:07 -0800

    remove the background color assignment from the js

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ for(let i = 0; i<gridsize; i++){
         tile.classList.add('tile');             //assign a classname to the tile so we can style it
         tile.style.height = '28px';
         tile.style.width = '28px';
-        tile.style.background='black';
         container.appendChild(tile); //adds our created tile to the grid
     }
     container.appendChild(document.createElement('br')); //add breakline after each row is complete
